20c5a9d918e2bbb810bdf633c9b2b7b3464a3d32
faliorith <165824600+faliorith@users.noreply.github.com>
2025-03-04 11:48:35 +0500

    Update first_screen.dart

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lab_5/second_screen.dart';
 
 class FirstScreen extends StatefulWidget {
   const FirstScreen({super.key});
@@ -11,20 +12,40 @@ class _FirstScreenState extends State< FirstScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Navigator'),
+      appBar: AppBar(title: Text('Home Screen')),
+      body: Column(children: [
+      ElevatedButton(
+        onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen()));
+        },
+        child: Text('Push'),
+        ),
+      ElevatedButton(
+        onPressed: (){
+          Navigator.pushNamed(context, '/second');
+        },
+        child: Text('PushNamed'),
       ),
-      body: Padding(padding: const EdgeInsets.all(40.0),
-      child: Column(
-        children: [
-        ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/second');
-            },
-           child: const Text("push"))
+      ElevatedButton(
+        onPressed: (){
+          Navigator.pushReplacement(context, MaterialPageRoute(builder:(context) => SecondScreen()));
+        },
+        child: Text('PushReplacement'),
+      ),
+      ElevatedButton(
+        onPressed: (){
+          Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => SecondScreen()), (Route<dynamic> route) => false);
+        },
+        child: Text('PushAndRemoveUntil'),
+      ),
+      ElevatedButton(
+        onPressed: () {
+          Navigator.pushNamedAndRemoveUntil(context, '/second', (route) => false);        
+          },
+        child: Text('PushNamedAndRemoveUntil'),
+      ),
       ],
-      ),
-      ),
-    );
+      )
+  );
   }
-}
+  }
